c7aef8f0627fc8442c54f07844b3f9d1fff88c2e
evanjuv <e.juav24@gmail.com>
2015-08-22 18:08:01 -0500

    Removes devise and sets new method for tumblr's authentication

--- Gemfile
+++ Gemfile
@@ -10,7 +10,6 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'semantic-ui-sass', github: 'doabit/semantic-ui-sass'
-gem 'devise'
 gem 'tumblr-api', :source => 'http://gemcutter.org'
 gem 'omniauth-tumblr'
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,9 @@
-class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,
-         :omniauthable, :omniauth_providers => [:tumblr]
-
- 	def self.from_omniauth(auth)
-	  where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-	    user.name = auth.info.name   
-	  end
-	end
-end
+class User < ActiveRecord::Base  
+  def self.create_with_omniauth(auth)  
+    create! do |user|  
+      user.provider = auth["provider"]  
+      user.uid = auth["uid"]  
+      user.name = auth["info"]["name"]  
+    end  
+  end  
+end  
