8ad564cf0ebed2a204f2a7a484bb17355c57f289
evanjuv <e.juav24@gmail.com>
2015-08-23 12:50:24 -0500

    Removes initializer for omniauth

--- config/initializers/omniauth.rb
+++
@@ -1,3 +0,0 @@
-    Rails.application.config.middleware.use OmniAuth::Builder do  
-      provider :tumblr, 'wUoVuaj6WJbMe4jLT1JXx1Pv14F8weXKm4cIXIGX1G1UKzde6R', 'zDCLgZ4sLx82GY08kMbf9JHUUHHtNtlJU4eSPKS6RvlvQ6jypY' 
-    end  
